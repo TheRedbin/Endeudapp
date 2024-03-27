112e3175a185f6819ee6185b064243b6b2cd633b
NicolasHtk <Nicolas1325@hotmail.com>
2024-03-26 22:03:56 -0500

    <Fix> Se resuelve bug con los decimales

--- script.js
+++ script.js
@@ -6,24 +6,21 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-const localStorageTransactions = JSON.parse(
-    localStorage.getItem('transactions')
-);
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
 
-let transactions =
-    localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
-// Add transaction
+// Agregar transacción
 function addTransaction(e) {
     e.preventDefault();
 
     if (text.value.trim() === '' || amount.value.trim() === '') {
-        alert('Please add a text and amount');
+        alert('Por favor ingresa un texto y un monto');
     } else {
         const transaction = {
             id: generateID(),
             text: text.value,
-            amount: + Number.parseFloat(amount.value)
+            amount: parseFloat(amount.value.replace(/\./g, '').replace(',', '.'))
         };
 
         transactions.push(transaction);
@@ -39,53 +36,41 @@ function addTransaction(e) {
     }
 }
 
-// Generate random ID
+// Generar ID aleatorio
 function generateID() {
     return Math.floor(Math.random() * 100000000);
 }
 
-// Add transactions to DOM list
+// Agregar transacciones al DOM
 function addTransactionDOM(transaction) {
-    // Get sign
     const sign = transaction.amount < 0 ? '-' : '+';
 
     const item = document.createElement('li');
 
-    // Add class based on value
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
     item.innerHTML = `
-    ${transaction.text} <span>${sign}${Math.abs(
-        transaction.amount
-    )}</span> <button class="delete-btn" onclick="removeTransaction(${transaction.id
-        })">x</button>
+    ${transaction.text} <span>${sign} ${Math.abs(transaction.amount).toLocaleString('es-ES', {minimumFractionDigits: 2, maximumFractionDigits: 2})}</span> <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
   `;
 
     list.appendChild(item);
 }
 
-// Update the balance, income and expense
+// Actualizar el balance, ingresos y gastos
 function updateValues() {
     const amounts = transactions.map(transaction => transaction.amount);
 
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const income = amounts.filter(item => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const expense = (amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
 
-    const income = amounts
-        .filter(item => item > 0)
-        .reduce((acc, item) => (acc += item), 0)
-        .toFixed(2);
-
-    const expense = (
-        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
-        -1
-    ).toFixed(2);
-
-    balance.innerText = `$${total}`;
-    money_plus.innerText = `$${income}`;
-    money_minus.innerText = `$${expense}`;
+    balance.innerText = `$${parseFloat(total).toLocaleString('es-ES', {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
+    money_plus.innerText = `$${parseFloat(income).toLocaleString('es-ES', {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
+    money_minus.innerText = `$${parseFloat(expense).toLocaleString('es-ES', {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
 }
 
-// Remove transaction by ID
+
+// Remover transacción por ID
 function removeTransaction(id) {
     transactions = transactions.filter(transaction => transaction.id !== id);
 
@@ -94,17 +79,39 @@ function removeTransaction(id) {
     init();
 }
 
-// Update local storage transactions
+// Actualizar transacciones en el almacenamiento local
 function updateLocalStorage() {
     localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
-// Init app
+// Inicializar la aplicación
 function init() {
     list.innerHTML = '';
-
     transactions.forEach(addTransactionDOM);
     updateValues();
+}
+
+
+//decimales en input
+function formatoNumero(input) {
+    // Obtener el valor actual del input
+    let valor = input.value.replace(/\D/g, ''); // Eliminar caracteres que no sean dígitos
+
+    // Formatear el número
+    valor = formatearNumero(valor);
+
+    // Asignar el valor formateado al input
+    input.value = valor;
+}
+
+function formatearNumero(amount) {
+    // Si el número es mayor o igual a 1000, formatearlo con separadores de miles
+    if (parseInt(amount) >= 1000) {
+        let numero = parseInt(amount); // Convertir a número
+        return numero.toLocaleString(); // Utilizar formato de separadores de miles del navegador
+    } else {
+        return amount;
+    }
 }
 
 init();
